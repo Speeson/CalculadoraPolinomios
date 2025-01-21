03dc18b66894a43bb060a4515fc47e8ba13e43ce
Steve <esteban_gperez@hotmail.com>
2025-01-21 16:10:06 +0100

    añadida opcion 5

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import TaylorPolinomioV2
 def mostrar_menu():
     print("\nMenu Principal")
     print("1. Generar Polinomio de Taylor")
-    print("2. Opción 2 (por definir)")
-    print("3. Opción 3 (por definir)")
-    print("4. Opción 4 (por definir)")
-    print("5. Salir")
+    print("2. Opción 2 (christian)")
+    print("3. Opción 3 (gonzalo")
+    print("4. Opción 4 (natalia)")
+    print("5. Opción 5 (carol)")
+    print("6. Salir")
 
 def main():
     while True:
@@ -20,16 +21,18 @@ def main():
             n = int(input('Introduzca el orden del polinomio de Taylor: '))
             TaylorPolinomioV2.PolTaylor(a, n, func_str)
         elif opcion == "2":
-            print("Opción 2 seleccionada (aún no implementada).")
+            print("Opción 2 aqui tu funcion chrissszzz (aún no implementada).")
         elif opcion == "3":
-            print("Opción 3 seleccionada (aún no implementada).")
+            print("Opción 3 aqui tu funcion marotooo (aún no implementada).")
         elif opcion == "4":
-            print("Opción 4 seleccionada (aún no implementada).")
+            print("Opción 4 aqui tu funcion nataliaaa (aún no implementada).")
         elif opcion == "5":
+            print("Opción 5 aqui tu funcion carooool (aún no implementada).")
+        elif opcion == "6":
             print("Saliendo del programa. ¡Hasta luego!")
             break
         else:
-            print("Opción no válida. Por favor, intente nuevamente.")
+            print("Opción no válida. Por favor, inténtelo de nuevo.")
 
 if __name__ == "__main__":
     main()
